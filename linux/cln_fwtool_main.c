ae0dea220fe925f21ae2c5ed321dbdbfafa85ddf
Lans Zhang <jia.zhang@windriver.com>
2015-11-25 10:38:18 +0800

    Change copyright info
    Signed-off-by: Lans Zhang <jia.zhang@windriver.com>

--- linux/cln_fwtool_main.c
+++ linux/cln_fwtool_main.c
@@ -1,29 +1,11 @@
 /*
  * Firmware Tool for Intel Clanton SoC
  *
- * Copyright (c) 2015, Lans Zhang <jia.zhang@windriver.com>
- * All rights reserved.
+ * Copyright (c) 2015 Wind River Systems, Inc.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * See "LICENSE" for license terms.
  *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Author: Lans Zhang <jia.zhang@windriver.com>
  */
 
 #include <eee.h>
@@ -43,8 +25,9 @@ show_banner(void)
 {
 	info_cont(T("\nFirmware Tool for Intel Clanton SoC ")
 		  T("(Intel Quark SoC X1000 series)\n"));
-	info_cont(T("Copyright (c) 2015, ")
-		  T("Lans Zhang <jia.zhang@windriver.com>\n"));
+	info_cont(T("Copyright (c) 2015 ")
+		  T("Wind River Systems, Inc.\n"));
+	info_cont(T("Author: Lans Zhang <jia.zhang@windriver.com>\n"));
 	info_cont(T("Version: %s\n\n"), T(VERSION));
 }
 
